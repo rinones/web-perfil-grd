ee628809604b96c88258db15d715b546d8b97be7
rinones <rinonesdiez@gmail.com>
2025-06-18 17:08:27 +0200

    Se retira navbar.

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navHamburguer = document.querySelector('#navHamburguer');
-    if (navHamburguer) {
-        navHamburguer.addEventListener('click', (e) => {
-            navHamburguer.parentElement.classList.toggle('active');
-        });
-    }
-});
